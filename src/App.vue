a950d12fa98252cba7e12836d52e8df17894aad9
Eugene Markusheuski <markuszewski.oz@gmail.com>
2022-10-18 21:47:54 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app">
+    <post-form @create="createPost"/>
+    <post-list :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JS", body: "descr of JS" },
+        { id: 2, title: "python", body: "descr of python" },
+        { id: 3, title: "C", body: "descr of C" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+    },
+  },
+  components: { PostForm, PostList }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post">
+    <div>
+      <div><strong>Name: </strong>{{post.title}}</div>
+      <div><strong>Description: </strong>{{post.body}}</div>
+    </div>
+    <my-button>Delete post</my-button>
+  </div>
+</template>
+
+<script>
+import MyButton from '@/components/UI/MyButton';
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    }
+  },
+  components: { MyButton }
+}
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
